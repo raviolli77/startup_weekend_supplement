480b8aa33ff741f41c3ff7ccf98bb195296def00
raviolli77 <rauleulogio3@gmail.com>
2018-02-21 00:49:23 -0800

    adding images for presentation

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ This repo serves as a supplement to my presentation on *Machine Learning* showca
 + Supervised Learning - [Customer Churn Prediction](/churn_prediction)
 
 More info to come
+
+## Resources
+
++ [IBM Customer Churn Source](https://www.ibm.com/communities/analytics/watson-analytics-blog/guide-to-sample-datasets/)
++ [Spotify Data Source](https://github.com/timothydnguyen/spotilyzer)
--- churn_prediction/script.R
+++ churn_prediction/script.R
@@ -1,79 +1,74 @@
 library(tidyverse)
 library(GGally)
 library(here)
-customer_churn <- read_csv(here::here("data", "customer-churn.csv"))
-
-View(customer_churn)
+customer_churn <- read_csv(here::here("churn_prediction", "customer-churn.csv"))
 
 customer_churn_no_id <- customer_churn %>% 
   select(-c(customerID))
 
-summary(customer_churn$TotalCharges)
-which(is.na(customer_churn$TotalCharges))
+customer_churn_no_id %>%
+  count(Churn)
 
-View(customer_churn[489, ])
-ggplot(customer_churn, 
-       aes(PhoneService, MonthlyCharges, fill = tenure)) + 
-  geom_raster()
+customer_churn_no_id %>%
+  filter(Churn == "Yes") %>%
+  count(Contract)
 
-ggpairs(customer_churn_no_id)
+customer_churn_no_id %>%
+  filter(Churn == "No") %>%
+  count(Contract)
 
-ggplot(customer_churn, aes(PaymentMethod, MonthlyCharges)) +
-  geom_boxplot() +
-  facet_wrap(~ Churn) +
-  theme_bw()
+# Exploring Features Based on Feature Importance for GBM Model 
 
-ggplot(customer_churn, aes(gender, MonthlyCharges)) +
-  geom_boxplot() +
-  facet_wrap(~ Churn) +
-  theme_bw()
+## Total Charges
+ggplot(customer_churn_no_id, 
+       aes(TotalCharges, fill=Contract)) + 
+  geom_histogram(bins = 100, 
+                 na.rm = TRUE) +
+  facet_wrap(~Churn) + 
+  theme_bw() + 
+  labs(x = "Total Charges", y = "Total") + 
+  ggtitle("Total Charges Vs. Churn \n(IBM Customer Churn Data Set)")
 
-ggplot(customer_churn, aes(Dependents, MonthlyCharges)) +
-  geom_boxplot() +
-  facet_wrap(~ Churn) +
-  theme_bw()
+# Monthly Charges
+ggplot(customer_churn_no_id, 
+       aes(MonthlyCharges, fill=Contract)) + 
+  geom_histogram(bins = 200, 
+                 na.rm = TRUE) +
+  facet_wrap(~Churn) + 
+  theme_bw() + 
+  labs(x = "Monthly Charges", y = "Total") + 
+  ggtitle("Monthly Charges Vs. Churn \n(IBM Customer Churn Data Set)")
 
+# Tenure
+ggplot(customer_churn_no_id, 
+       aes(tenure, fill=Contract)) + 
+  geom_histogram(bins=75, 
+                 na.rm = TRUE) +
+  facet_wrap(~Churn) + 
+  theme_bw() + 
+  labs(x = "Tenure", y = "Total") + 
+  ggtitle("Tenure Vs. Churn Comparison \n(IBM Customer Churn Data Set)")
+
+# More Exploration on Data set
+ggplot(customer_churn,
+       aes(tenure, TotalCharges,
+           colour = PhoneService)) +
+  geom_point(na.rm = TRUE) + 
+  theme_minimal() + 
+  labs(x = "Tenure", y = "Total Charges") + 
+  ggtitle("Tenure Vs. Total Charges \n(IBM Customer Churn Data Set)")
 
 ggplot(customer_churn,
        aes(tenure, TotalCharges,
            colour = PhoneService)) +
-  geom_point(na.rm = TRUE)
-
-# PHONE SERVICES
-ggplot(customer_churn,
-       aes(tenure, TotalCharges,
-           colour = PhoneService)) +
-  geom_point(na.rm = TRUE) +
-  theme_minimal()
+  geom_point(na.rm = TRUE) + 
+  facet_grid(InternetService ~ Churn) + 
+  theme_bw()
 
 # FACET WRAPPING TO SEE MORE PATTERNS
 ggplot(customer_churn,
        aes(tenure, TotalCharges,
-           colour = PhoneService)) +
+           colour = InternetService)) +
   geom_point(na.rm = TRUE) +
   facet_wrap( ~ PhoneService) +
-  theme_bw()
-
-#
-ggplot(customer_churn,
-       aes(tenure, TotalCharges,
-           colour = PhoneService)) +
-  geom_point(na.rm = TRUE) +
-  facet_grid(Churn ~ InternetService) +
-  theme_bw()
-
-
-ggplot(customer_churn,
-       aes(tenure, MonthlyCharges,
-           colour = PhoneService)) +
-  geom_point(na.rm = TRUE) +
-  facet_wrap( ~ Churn) +
-  theme_bw()
-
-
-ggplot(customer_churn,
-       aes(TotalCharges, MonthlyCharges,
-           colour = Contract)) +
-  geom_point(na.rm = TRUE) +
-  facet_wrap( ~ Churn) +
   theme_bw()
